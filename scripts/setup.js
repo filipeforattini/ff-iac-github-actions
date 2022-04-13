4d888692849dcaf0e91591e441e511896b30851f
Filipe Forattini <filipe@vitta.me>
2022-04-13 08:49:39 -0300

    test

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,18 +1,27 @@
 const git = require('./src/git')
 
-module.exports = ({ github, context }) => console.log({
-  githubJson: JSON.stringify(github),
-  contextJson: JSON.stringify(context),
-
-  // git related
-  fromRepository: {
-    branch: git.branch(),
-    commit: git.commitSha(),
+module.exports = ({ github, context }) => console.log(JSON.stringify({
+  // time related
+  time: {
+    stepTimestamp: Date.now(),
+    date: context.payload.head_commit.timestamp.substring(0, 10),
+    timestamp: new Date(context.payload.head_commit.timestamp).getTime(),
   },
 
-  fromContext: {
-    branch: context.ref.replace('/refs/heads/', ''),
+  // git related
+  git: {
+    branch: context.ref.replace('refs/heads/', ''),
     commit: context.sha.substring(0, 7),
+
+    fromRepository: {
+      branch: git.branch(),
+      commit: git.commitSha(),
+    },
+  
+    fromContext: {
+      branch: context.ref.replace('refs/heads/', ''),
+      commit: context.sha.substring(0, 7),
+    },
   },
 
   // run related
@@ -22,10 +31,16 @@ module.exports = ({ github, context }) => console.log({
     event: context.eventName,
     repository: context.workflow,
   },
-  
-  // time related
-  time: {
-    timestamp: Date.now(),
-    date: new Date().toISOString().substring(0, 10),
+
+  // actor related
+  actor: {
+    ...context.payload.head_commit.commiter,
+    firstName: context.payload.head_commit.commiter.split(' ').shift(),
+    messages: context.payload.commits.map(c => c.message),
   },
-})
+
+  // pipeline related
+  config: {
+    type: 'service',
+  }
+}))
