d2b642ee645dc349f1ad670b3dae2d4f6c5e6939
Filipe Forattini <filipe@vitta.me>
2022-04-13 19:07:52 -0300

    test

--- src/scrappers/helm.class.js
+++ src/scrappers/helm.class.js
@@ -2,7 +2,7 @@ const Scrapper = require('./scrapper.class')
 
 module.exports = class Helm extends Scrapper {
   setup () {
-    const repository = this.output.repository.name
+    const repository = this.context.payload.repository.full_name
 
     const pipeline = repository.includes('-svc-')
       ? 'svc'
