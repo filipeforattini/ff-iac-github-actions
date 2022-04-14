bc58d301660493f09afd33f33f08adab8ef77916
Filipe Forattini <filipe@vitta.me>
2022-04-14 08:53:09 -0300

    test

---
+++ src/scrappers/docker.class.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const path = require('path')
+const Scrapper = require('./scrapper.class')
+
+module.exports = class Docker extends Scrapper {
+  setup () {
+    const hasDockerfile = fs.existsSync(path.join(process.cwd(), 'dockerfile'))
+      ? true
+      : false
+
+    const hasDockerignore = fs.existsSync(path.join(process.cwd(), '.dockerignore'))
+      ? true
+      : false
+
+    const containerRegistry = this.inputs.containerRegistry
+    const containerName = this.context.payload.repository.full_name
+
+    this
+      .add('dockerfile', {
+        hasDockerfile,
+        hasDockerignore,
+        tags: [
+          `${containerRegistry}/${containerName}:latest`,
+          `${containerRegistry}/${containerName}:d-${this.output.run.date}`,
+          `${containerRegistry}/${containerName}:b-${this.output.git.branch}`,
+          `${containerRegistry}/${containerName}:c-${this.output.git.commit}`,
+          `${containerRegistry}/${containerName}:t-${this.output.run.timestamp}`,
+          // `${containerRegistry}/${containerName}:node-${matrix.node-version}`,
+          // `${containerRegistry}/${containerName}:node-${matrix.node-version}-latest`,
+          // `${containerRegistry}/${containerName}:node-${matrix.node-version}-d-${needs.Setup.outputs.Date}`,
+          // `${containerRegistry}/${containerName}:node-${matrix.node-version}-b-${needs.Setup.outputs.Branch}`,
+          // `${containerRegistry}/${containerName}:node-${matrix.node-version}-c-${needs.Setup.outputs.ShaHash}`,  
+        ]
+      })
+  }
+}
