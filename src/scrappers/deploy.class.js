43a839812c711400792656c18b993909e7ff2f31
Filipe Forattini <filipe@vitta.me>
2022-04-26 11:10:46 -0300

    test

---
+++ src/scrappers/deploy.class.js
@@ -0,0 +1,37 @@
+const Scrapper = require('./scrapper.class')
+
+module.exports = class Deploy extends Scrapper {
+  setup () {
+    const repository = this.context.payload.repository.full_name
+
+    const pipeline = repository.includes('-svc-')
+      ? 'svc'
+      : repository.includes('-app-')
+        ? 'app'
+        : repository.includes('-iac-')
+          ? 'iac'
+          : 'unknown'
+
+
+    const containerRegistry = this.inputs.containerRegistry
+    const containerName = this.context.payload.repository.full_name
+
+    const deployAsK8s = fs.existsSync(path.join(process.cwd(), 'manifests', 'k8s-values.yml'))
+      ? true
+      : false
+
+    const deployAsChart = fs.existsSync(path.join(process.cwd(), 'manifests', 'charts-values.yml'))
+      ? true
+      : false
+    this
+      .add('helm', {
+        pipeline,
+        deployAsK8s,
+        deployAsChart,
+        containerName,
+        pod: pipeline,
+        containerRegistry,
+        namespace: repository,
+      })
+  }
+}
