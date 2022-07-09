89f70f328adcb3c15e3b04fa4a785ad7a574e267
Filipe Forattini <filipe@vitta.me>
2022-07-09 19:01:31 -0300

    fix: empty commit for testing

--- src/actions/pipeline-config-scrapper/index.js
+++ src/actions/pipeline-config-scrapper/index.js
@@ -6,13 +6,6 @@ const linguist = require("linguist-js");
 async function action () {
   await core.summary
     .addHeading('Analized 0')
-    .addDetails('context', JSON.stringify(github.context, null, 2))
-    .addCodeBlock(`
-graph event-push;
-eventpush-->Analyze;
-Analyze-->StaticAnalizys;
-StaticAnalizys-->Test;
-Test-->Release;`,'mermaid')
     .write()
 
   core.info(JSON.stringify(github.context, null, 2));
