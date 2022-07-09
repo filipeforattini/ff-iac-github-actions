1a5c00c13a6c8f0a03a842db568c2e569b20ce36
Filipe Forattini <filipe@vitta.me>
2022-07-09 13:28:34 -0300

    feat: using actions in root dir

---
+++ actions/pipeline-config-scrapper/src/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  Git: require('./git.class'),
+  Run: require('./run.class'),
+  Deploy: require('./deploy.class'),
+  
+  // code related
+  Code: require('./code.class'),
+  Nodejs: require('./nodejs.class'),
+  Python: require('./python.class'),
+  Docker: require('./docker.class'),
+}
---
+++ actions/pipeline-config-scrapper/src/scrapper.class.js
@@ -0,0 +1,34 @@
+const mergeDeep = require('../../../src/merge-deep')
+
+module.exports = class Scrapper {
+  constructor ({ github, context, inputs, core, glob, io, exec }, output) {
+    this.io = io
+    this.core = core
+    this.exec = exec
+    this.glob = glob
+    this.inputs = inputs
+    this.github = github
+    this.output = output
+    this.context = context
+
+    this.data = {}
+  }
+
+  static load (...args) {
+    const instance = new this(...args)
+    instance.setup()
+    return instance
+  }
+
+  add (key, data) {
+    this.data[key] = mergeDeep(
+      this.data[key] || {},
+      data
+    )
+
+    return this
+  }
+
+  // overwrite
+  setup () {}
+}
