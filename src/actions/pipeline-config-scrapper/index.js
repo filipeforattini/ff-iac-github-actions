f29f56c03d23676602a830358b067c9d336bf6b7
Filipe Forattini <filipe@vitta.me>
2022-07-09 18:49:37 -0300

    fix: empty commit for testing

--- src/actions/pipeline-config-scrapper/index.js
+++ src/actions/pipeline-config-scrapper/index.js
@@ -3,50 +3,65 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const linguist = require("linguist-js");
 
-const {
-  Git, 
-  Run, 
-  Code, 
-  Deploy,
-  Docker,
-  Nodejs,
-  Python,
-} = require('./src')
-
 try {
-  let providers = [ Git, Run, Code ]
-  let output = {}
-  let args = github.context
   core.info(JSON.stringify(github.context, null, 2));
 
-  output = providers
-    .map(p => p.load(args, output))
-    .reduce((acc, i) => _.merge(acc, i.data), {})
+  core.summary.addHeading('Analized 0', 0)
+  core.summary.addHeading('Analized 1', 1)
+  core.summary.addHeading('Analized 2', 2)
+  core.summary.addHeading('Analized 3', 3)
+  core.summary.addHeading('Analized 4', 4)
+  
+  core.summary.addDetails('context', JSON.stringify(github.context, null, 2))
 
-  if (output.code.isNode) providers = [ Nodejs ]
-  if (output.code.isPython) providers = [ Python ]
+  core.summary.addCodeBlock(`
+graph event-push;
+eventpush-->Analyze;
+Analyze-->StaticAnalizys;
+StaticAnalizys-->Test;
+Test-->Release;
+  `,'mermaid')
 
-  output = providers
-    .map(p => p.load(args, output))
-    .reduce((acc, i) => _.merge(acc, i.data), output)
 
-  providers = [ Docker, Deploy ]
 
-  output = providers.map(p => p.load(args, output))
-    .reduce((acc, i) => _.merge(acc, i.data), output)
 
-  core.setOutput("config", JSON.stringify(output));
 
-  const { languages } = linguist(process.cwd(), {
-    categories: ["programming"],
-  });
 
-  console.log(`languages: ${JSON.stringify(languages, null, 2)}`);
 
-  core.setOutput("language", languages);
-  const payload = JSON.stringify(github.context, undefined, 2);
 
-  console.log(`The event payload: ${payload}`);
+
+  // let providers = [ Git, Run, Code ]
+  // let output = {}
+  // let args = github.context
+
+  // output = providers
+  //   .map(p => p.load(args, output))
+  //   .reduce((acc, i) => _.merge(acc, i.data), {})
+
+  // if (output.code.isNode) providers = [ Nodejs ]
+  // if (output.code.isPython) providers = [ Python ]
+
+  // output = providers
+  //   .map(p => p.load(args, output))
+  //   .reduce((acc, i) => _.merge(acc, i.data), output)
+
+  // providers = [ Docker, Deploy ]
+
+  // output = providers.map(p => p.load(args, output))
+  //   .reduce((acc, i) => _.merge(acc, i.data), output)
+
+  // core.setOutput("config", JSON.stringify(output));
+
+  // const { languages } = linguist(process.cwd(), {
+  //   categories: ["programming"],
+  // });
+
+  // console.log(`languages: ${JSON.stringify(languages, null, 2)}`);
+
+  // core.setOutput("language", languages);
+  // const payload = JSON.stringify(github.context, undefined, 2);
+
+  // console.log(`The event payload: ${payload}`);
 } catch (error) {
 
   core.setFailed(error.message);
