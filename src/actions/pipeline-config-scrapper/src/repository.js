5b2bd511551794231ca431da6b8d706aa6f3e519
Filipe Forattini <filipe@vitta.me>
2022-07-12 18:08:37 -0300

    fix: empty commit for testing

--- src/actions/pipeline-config-scrapper/src/repository.js
+++ src/actions/pipeline-config-scrapper/src/repository.js
@@ -5,8 +5,10 @@ module.exports = async (analysis) => {
   analysis.features = {
     hasReleaseRC: fs.existsSync(path.join(analysis.root, '.releaserc.json')),
     hasDockerfile: fs.existsSync(path.join(analysis.root, 'Dockerfile')),
+    hasDockerignore: fs.existsSync(path.join(analysis.root, '.dockerignore')),
   }
 
   analysis.outputs.feature_has_releaserc = analysis.features.hasReleaseRC
   analysis.outputs.feature_has_dockerfile = analysis.features.hasDockerfile
+  analysis.outputs.feature_has_dockerignore = analysis.features.hasDockerignore
 }
