59cffe9644fa41f80b09c5d71e5ef55626740c51
Filipe Forattini <filipe@vitta.me>
2022-07-13 09:29:11 -0300

    fix: empty commit for testing

--- src/actions/create-dockerfile/stubs/index.js
+++ src/actions/create-dockerfile/stubs/index.js
@@ -1,7 +1,4 @@
-const fs = require('fs')
-const path = require('path')
-
 module.exports = {
-  python: fs.readFileSync(path.join(__dirname, 'python')).toString(),
-  javascript: fs.readFileSync(path.join(__dirname, 'javascript')).toString(),
+  python: require('./python'),
+  javascript: require('./javascript'),
 }
