e35c6331fda4f225639a6a81f9e5b79a8b176935
Filipe Forattini <filipe@vitta.me>
2022-07-17 13:50:39 -0300

    fix: empty commit for testing

--- src/actions/config-scrapper/src/deployment.js
+++ src/actions/config-scrapper/src/deployment.js
@@ -3,18 +3,21 @@ const github = require("@actions/github");
 module.exports = async (analysis) => {
   let containerRegistry = core.getInput('containerRegistry', { required: true });
 
+  const [ organization, name ] = github.context.payload.repository.full_name.split('/')
+  const imageFullName = [ containerRegistry, organization, name].join('/')
+
   if (github.context.payload.deployment) {
     analysis.environment = github.context.payload.deployment.environment
     analysis.outputs.environment = analysis.environment
   }
 
   analysis.deployment.tags = [
-    `${imageFullname}:latest`,
-    `${imageFullname}:d-${this.output.run.date}`,
-    `${imageFullname}:r-${this.output.run.count}`,
-    `${imageFullname}:t-${this.output.run.startTimestamp}`,
-    `${imageFullname}:b-${this.output.git.branch.replace('/', '-')}`,
-    `${imageFullname}:c-${this.output.git.commit}`,
+    `${imageFullName}:latest`,
+    `${imageFullName}:d-${this.output.run.date}`,
+    `${imageFullName}:r-${this.output.run.count}`,
+    `${imageFullName}:t-${this.output.run.startTimestamp}`,
+    `${imageFullName}:b-${this.output.git.branch.replace('/', '-')}`,
+    `${imageFullName}:c-${this.output.git.commit}`,
     // `${imageFullName}:node-${matrix.node-version}`,
     // `${imageFullName}:node-${matrix.node-version}-latest`,
     // `${imageFullName}:node-${matrix.node-version}-d-${needs.Setup.outputs.Date}`,
