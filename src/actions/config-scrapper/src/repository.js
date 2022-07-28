2ba6b3a6e888e9f58c146b85093ce18444383ec1
Filipe Forattini <filipe@vitta.me>
2022-07-28 20:36:29 -0300

    feat: added ecosystem exporter

--- src/actions/config-scrapper/src/repository.js
+++ src/actions/config-scrapper/src/repository.js
@@ -1,26 +1,33 @@
-const fs = require('fs')
-const path = require('path')
-const github = require("@actions/github")
+const fs = require("fs");
+const path = require("path");
+const github = require("@actions/github");
 
 module.exports = async (analysis) => {
-  const [ organization, name ] = github.context.payload.repository.full_name.split('/')
+  const [organization, name] = github.context.payload.repository.full_name.split("/");
+
+  let [ecosystem, type, ...client] = name.split("-");
+  client = client.join('-'),
 
   analysis.repository = {
     name,
     organization,
     fullname: github.context.payload.repository.full_name,
-  }
+    ecosystem,
+    type,
+    client
+  };
 
   analysis.features = {
-    hasReleaseRC: fs.existsSync(path.join(analysis.root, '.releaserc.json')),
-    hasDockerfile: fs.existsSync(path.join(analysis.root, 'Dockerfile')),
-    hasDockerignore: fs.existsSync(path.join(analysis.root, '.dockerignore')),
-  }
-  
+    hasReleaseRC: fs.existsSync(path.join(analysis.root, ".releaserc.json")),
+    hasDockerfile: fs.existsSync(path.join(analysis.root, "Dockerfile")),
+    hasDockerignore: fs.existsSync(path.join(analysis.root, ".dockerignore")),
+  };
+
   // outputs
-  analysis.outputs.repository = analysis.repository.name
-  analysis.outputs.organization = analysis.repository.organization
-  analysis.outputs.feature_has_releaserc = analysis.features.hasReleaseRC
-  analysis.outputs.feature_has_dockerfile = analysis.features.hasDockerfile
-  analysis.outputs.feature_has_dockerignore = analysis.features.hasDockerignore
-}
+  analysis.outputs.ecosystem = analysis.repository.ecosystem;
+  analysis.outputs.repository = analysis.repository.name;
+  analysis.outputs.organization = analysis.repository.organization;
+  analysis.outputs.feature_has_releaserc = analysis.features.hasReleaseRC;
+  analysis.outputs.feature_has_dockerfile = analysis.features.hasDockerfile;
+  analysis.outputs.feature_has_dockerignore = analysis.features.hasDockerignore;
+};
