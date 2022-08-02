547a85d3799a927437292adb300ea791186e4c85
Filipe Forattini <filipe@vitta.me>
2022-08-02 13:05:52 -0300

    fix: dockerfile for python

--- src/actions/config-scrapper/src/code.js
+++ src/actions/config-scrapper/src/code.js
@@ -2,37 +2,40 @@ const _ = require("lodash");
 const core = require("@actions/core");
 const linguist = require("linguist-js");
 
-const { templateInfo } = require('../log')
+const { templateInfo } = require("../log");
 
+const LanguagesToOmit = ["Makefile"];
 const languagesRouter = {
-  typescript: 'javascript',
-}
+  typescript: "javascript",
+};
 
 module.exports = async (analysis) => {
-  const { languages } = await linguist(analysis.root, {
-    categories: [ "programming" ],
-    ignoredLanguages: [ "Shell", "Dockerfile" ],
+  let { languages } = await linguist(analysis.root, {
+    categories: ["programming"],
+    ignoredLanguages: ["Shell", "Dockerfile"],
   });
 
-  analysis.code.languages = languages.results
+  languages = _.omit(languages.results, LanguagesToOmit)
+  analysis.code.languages = languages;
 
-  let langIterator = _.mapValues(languages.results, "bytes");
+  let langIterator = _.mapValues(languages, "bytes");
   langIterator = _.toPairs(langIterator);
   langIterator = langIterator.map((z) => _.zipObject(["language", "bytes"], z));
   langIterator = _.sortBy(langIterator, "bytes");
 
-  if (langIterator.length == 0) return core.warning(templateInfo('code', "no language detected"));
+  if (langIterator.length == 0)
+    return core.warning(templateInfo("code", "no language detected"));
 
   let language = langIterator.pop().language;
   language = language.toLowerCase();
-  
-  core.info(templateInfo('code', `language ${language} detected!`));
-  
+
+  core.info(templateInfo("code", `language ${language} detected!`));
+
   if (languagesRouter[language]) {
-    language = languagesRouter[language]
-    core.info(templateInfo('code', `language routed to ${language}!`));
+    language = languagesRouter[language];
+    core.info(templateInfo("code", `language routed to ${language}!`));
   }
-  
-  analysis.language = language
-  analysis.outputs.language = analysis.language
+
+  analysis.language = language;
+  analysis.outputs.language = analysis.language;
 };
