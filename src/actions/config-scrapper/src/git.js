adc5d646ec03c938dc6ca9188adec2b3c331f5c2
Filipe Forattini <filipe@vitta.me>
2022-08-02 15:11:10 -0300

    fix: empty commit for testing

--- src/actions/config-scrapper/src/git.js
+++ src/actions/config-scrapper/src/git.js
@@ -1,4 +1,5 @@
 const github = require("@actions/github");
+const dayjs = require('dayjs')
 
 module.exports = async (analysis) => {
   const commitSha = github.context.sha.substring(0,7)
@@ -11,10 +12,10 @@ module.exports = async (analysis) => {
     analysis.outputs.commiter_email = analysis.commiter.email
     analysis.outputs.commiter_username = analysis.commiter.username
 
-    analysis.outputs.date = new Date(github.context.payload.head_commit.timestamp)
+    analysis.outputs.executed_at = dayjs(github.context.payload.head_commit.timestamp)
   }
 
   if (github.context.payload.deployment) {
-    analysis.outputs.date = new Date(github.context.payload.deployment.updated_at)
+    analysis.outputs.executed_at = dayjs(github.context.payload.deployment.updated_at)
   }
 }
