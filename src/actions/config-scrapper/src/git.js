fd26cc919d478df000a8baed80372e1cf6047a72
Filipe Forattini <filipe@vitta.me>
2022-08-02 14:54:51 -0300

    fix: empty commit for testing

--- src/actions/config-scrapper/src/git.js
+++ src/actions/config-scrapper/src/git.js
@@ -3,12 +3,18 @@ const github = require("@actions/github");
 module.exports = async (analysis) => {
   const commitSha = github.context.sha.substring(0,7)
   analysis.outputs.commit_hash = commitSha
-  
+
   if (github.context.payload.head_commit) {
     analysis.commiter = { ...github.context.payload.head_commit.committer}
 
     analysis.outputs.commiter_name = analysis.commiter.name
     analysis.outputs.commiter_email = analysis.commiter.email
     analysis.outputs.commiter_username = analysis.commiter.username
+
+    analysis.outputs.date = new Date(github.context.payload.head_commit.timestamp)
+  }
+
+  if (github.context.payload.deployment) {
+    analysis.outputs.date = new Date(github.context.payload.deployment.updated_at)
   }
 }
