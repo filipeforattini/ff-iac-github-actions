b6a88ccc7790cb1177059b58c1e3277fa2f2a01d
Filipe Forattini <filipe@vitta.me>
2022-08-09 14:04:40 -0300

    feat: added dispatch command

--- src/actions/config-scrapper/src/event-deployment.js
+++ src/actions/config-scrapper/src/event-deployment.js
@@ -1,8 +1,13 @@
 const github = require("@actions/github");
 
+const { templateInfo } = require('../log')
+
 module.exports = async (analysis) => {
   if (github.context.payload.deployment) {
-    analysis.environment = github.context.payload.deployment.environment
-    analysis.outputs.environment = analysis.environment
+    const environment = github.context.payload.deployment.environment
+    templateInfo('e.deployment', `detected environment = ${environment}`)
+
+    analysis.environment = environment
+    analysis.outputs.environment = environment
   }
 }
--- src/actions/config-scrapper/src/event-dispatch.js
+++ src/actions/config-scrapper/src/event-dispatch.js
@@ -1,7 +1,10 @@
 const github = require("@actions/github");
 
+const { templateInfo } = require('../log')
+
 module.exports = async (analysis) => {
   const environment = github.context.payload.environment
+  templateInfo('e.dispatch', `detected environment = ${environment}`)
 
   analysis.environment = environment
   analysis.outputs.environment = environment
