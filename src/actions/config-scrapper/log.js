b4e57867c7ae897b40cbee685eecbc2b1a356048
Filipe Forattini <filipe@vitta.me>
2022-08-09 14:38:13 -0300

    feat: added dispatch command

--- src/actions/config-scrapper/log.js
+++ src/actions/config-scrapper/log.js
@@ -1,7 +1,7 @@
 const _ = require('lodash')
 
 module.exports = {
-  templateInfo: (context, ...args) => ':: ' +  _.pad(context, 12) + ' | ' + args.join('\n'),
+  templateInfo: (context, ...args) => ':: ' +  _.pad(context, 13) + ' | ' + args.join('\n'),
 
   templateDetails: _.template(`<details>
 <summary><%= summary %></summary>
