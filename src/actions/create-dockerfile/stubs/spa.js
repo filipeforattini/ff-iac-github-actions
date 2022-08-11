8470aa275a48adde5afaa382324ebdabd11a49a2
Filipe Forattini <filipe@vitta.me>
2022-08-11 09:10:53 -0300

    feat: added param for build directory

--- src/actions/create-dockerfile/stubs/spa.js
+++ src/actions/create-dockerfile/stubs/spa.js
@@ -15,7 +15,7 @@ FROM nginx:alpine
 COPY ./nginx.conf /etc/nginx/nginx.conf
 
 RUN rm -rf /usr/share/nginx/html/*
-COPY --from=builder /app/build /usr/share/nginx/html
+COPY --from=builder /app/<%= buildDirectory %> /usr/share/nginx/html
 
 EXPOSE 80
 
@@ -23,7 +23,9 @@ ENTRYPOINT ["nginx"]
 CMD ["-g daemon off;"]
 `,
 
-  defaultValues: {},
+  defaultValues: {
+    buildDirectory: 'build'
+  },
 
   files: {
     'nginx.conf': `
