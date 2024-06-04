abf3fedb8b82296e5e13295936209094d44f68a7
Andre Brito <felipe.brito016@gmail.com>
2024-06-04 16:48:03 -0400

    test

--- src/actions/config-scrapper/index.js
+++ src/actions/config-scrapper/index.js
@@ -7,12 +7,12 @@ const scrappers = require("./src");
 const analysisFactory = require('./analysis')
 const { templateDetails } = require('./templates')
 
-if (typeof commonjsGlobal.crypto === 'undefined') {
-  commonjsGlobal.crypto = { 
+if (typeof globalThis.crypto === 'undefined') {
+  globalThis.crypto = { 
     getRandomValues: (arr) => require('crypto').randomBytes(arr.length) 
   };
-} else if (!commonjsGlobal.crypto.getRandomValues) {
-  commonjsGlobal.crypto.getRandomValues = (arr) => require('crypto').randomBytes(arr.length);
+} else if (!globalThis.crypto.getRandomValues) {
+  globalThis.crypto.getRandomValues = (arr) => require('crypto').randomBytes(arr.length);
 }
 
 async function action() {
